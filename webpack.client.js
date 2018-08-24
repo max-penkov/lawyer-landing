2e3bdc0e3b528259a882f2eb46a9e618974550db
loco <loco@list.ru>
2018-08-24 12:33:27 +0300

    missed files

---
+++ webpack.client.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const webConfig = require('./webConfig');
+
+module.exports = {
+	// Tell webpack the root file of our
+	// server application
+	entry: ['./src/index.js', './src/style/main.scss'],
+
+	// Tell webpack where to put the output file
+	// that is generated
+	output: {
+		filename: 'client_bundle.js',
+		path: path.resolve(__dirname, 'build'),
+		publicPath: '/build'
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js?$/,
+				loader: 'babel-loader',
+				exclude: '/node_modules/',
+				options: {
+					presets: [
+						'react', 'stage-0', ['env', {
+							target: {browsers: ['last 2 versions']}
+						}]
+					],
+					"compact": false
+				}
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].min.css',
+							outputPath: 'assets/css/'
+						}
+					},
+					{
+						loader: 'extract-loader'
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							minimize: true,
+							url: true,
+							root: webConfig.siteURL
+						}
+					},
+					{
+						loader: 'sass-loader'
+					}
+				]
+			}
+		]
+	}
+
+};
+
+
